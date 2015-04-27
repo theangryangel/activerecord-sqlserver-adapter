2066ddfcb22c72ddebdd968a76d0bfc1ac4f906e
Karl Southern <karl@theangryangel.co.uk>
2015-04-27 15:03:29 +0100

    Fixes up database tasks to correctly export table definitions to SQL and also adds SPs. Fixes up backport issue with missing SQLServer utils.

--- lib/active_record/connection_adapters/sqlserver/database_tasks.rb
+++ lib/active_record/connection_adapters/sqlserver/database_tasks.rb
@@ -4,7 +4,7 @@ module ActiveRecord
       module DatabaseTasks
 
         def create_database(database, options = {})
-          name = SQLServer::Utils.extract_identifiers(database)
+          name = quote_database_name(database)
           options = {collation: @connection_options[:collation]}.merge!(options.symbolize_keys)
           options = options.select { |_, v| v.present? }
           option_string = options.inject("") do |memo, (key, value)|
@@ -19,7 +19,7 @@ module ActiveRecord
         end
 
         def drop_database(database)
-          name = SQLServer::Utils.extract_identifiers(database)
+          name = quote_database_name(database)
           do_execute "DROP DATABASE #{name}"
         end
 
--- lib/active_record/tasks/sqlserver_database_tasks.rb
+++ lib/active_record/tasks/sqlserver_database_tasks.rb
@@ -49,30 +49,73 @@ module ActiveRecord
       end
 
       def structure_dump(filename)
-        command = [
-          "defncopy",
-          "-S #{Shellwords.escape(configuration['host'])}",
-          "-D #{Shellwords.escape(configuration['database'])}",
-          "-U #{Shellwords.escape(configuration['username'])}",
-          "-P #{Shellwords.escape(configuration['password'])}",
-          "-o #{Shellwords.escape(filename)}",
-        ]
-        table_args = connection.tables.map { |t| Shellwords.escape(t) }
-        command.concat(table_args)
-        view_args = connection.views.map { |v| Shellwords.escape(v) }
-        command.concat(view_args)
-        raise 'Error dumping database' unless Kernel.system(command.join(' '))
-        dump = File.read(filename)
-        dump.gsub!(/^USE .*$\nGO\n/, '')                      # Strip db USE statements
-        dump.gsub!(/^GO\n/, '')                               # Strip db GO statements
-        dump.gsub!(/nvarchar\(8000\)/, 'nvarchar(4000)')      # Fix nvarchar(8000) column defs
-        dump.gsub!(/nvarchar\(-1\)/, 'nvarchar(max)')         # Fix nvarchar(-1) column defs
-        dump.gsub!(/text\(\d+\)/, 'text')                     # Fix text(16) column defs
-        File.open(filename, "w") { |file| file.puts dump }
+        File.open(filename, 'w') { |file|
+          # Generate DDL for tables
+          connection.select_all("select 
+  'create table [' + so.name + '] (' + o.list + ')' + CASE WHEN tc.Constraint_Name IS NULL THEN '' ELSE 'ALTER TABLE ' + so.Name + ' ADD CONSTRAINT ' + tc.Constraint_Name  + ' PRIMARY KEY ' + ' (' + LEFT(j.List, Len(j.List)-1) + ')' END as definition
+from sysobjects so
+cross apply
+    (SELECT 
+        '  ['+column_name+'] ' + 
+        data_type + case data_type
+            when 'sql_variant' then ''
+            when 'text' then ''
+            when 'ntext' then ''
+            when 'xml' then ''
+            when 'decimal' then '(' + cast(numeric_precision as varchar) + ', ' + cast(numeric_scale as varchar) + ')'
+            else coalesce('('+case when character_maximum_length = -1 then 'MAX' else cast(character_maximum_length as varchar) end +')','') end + ' ' +
+        case when exists ( 
+        select id from syscolumns
+        where object_name(id)=so.name
+        and name=column_name
+        and columnproperty(id,name,'IsIdentity') = 1 
+        ) then
+        'IDENTITY(' + 
+        cast(ident_seed(so.name) as varchar) + ',' + 
+        cast(ident_incr(so.name) as varchar) + ')'
+        else ''
+        end + ' ' +
+         (case when IS_NULLABLE = 'No' then 'NOT ' else '' end ) + 'NULL ' + 
+          case when information_schema.columns.COLUMN_DEFAULT IS NOT NULL THEN 'DEFAULT '+ information_schema.columns.COLUMN_DEFAULT ELSE '' END + ', ' 
+
+     from information_schema.columns where table_name = so.name
+     order by ordinal_position
+    FOR XML PATH('')) o (list)
+left join
+    information_schema.table_constraints tc
+on  tc.Table_name       = so.Name
+AND tc.Constraint_Type  = 'PRIMARY KEY'
+cross apply
+    (select '[' + Column_Name + '], '
+     FROM   information_schema.key_column_usage kcu
+     WHERE  kcu.Constraint_Name = tc.Constraint_Name
+     ORDER BY
+        ORDINAL_POSITION
+     FOR XML PATH('')) j (list)
+where   xtype = 'U'
+AND name NOT IN ('dtproperties')").each do |row|
+            file.puts "#{row['definition']}\r\nGO\r\n"
+          end
+
+          # Export views definitions
+          connection.select_all("select definition, o.type from sys.objects as o join sys.sql_modules as m on m.object_id = o.object_id where o.type = 'V'").each do |row|
+            file.puts "#{row['definition']}\r\nGO\r\n"
+          end
+
+          # Export stored procedures definitions
+          connection.select_all("select routine_definition as definition from information_schema.routines").each do |row|
+            file.puts "#{row['definition']}\r\nGO\r\n"
+          end
+        }
       end
 
       def structure_load(filename)
-        connection.execute File.read(filename)
+        structure = File.read(filename)
+        # Split by GO so that operations that must be in separate batches are in
+        # separate batches
+        structure.split(/^GO/).each { |s|
+          connection.execute s
+        }
       end
 
 
